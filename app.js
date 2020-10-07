bc4cd7e224ac79cb3576906d6758c3daa8fa9cf9
ivannabandura <cY2aH9zsidUmyaX>
2020-10-07 19:03:47 +0300

    menu

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 $(function () {
+  /* Fixed Header */
   let header = $("#header");
   let intro = $("#intro");
-  let introH;
+  let introH = intro.innerHeight();
   let scrollPos = $(window).scrollTop();
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
 
-  $(window).on("scroll load resize", function () {
+  checkScroll(scrollPos, introH);
+
+  $(window).on("scroll resize", function () {
     introH = intro.innerHeight();
     scrollPos = $(this).scrollTop();
 
+    checkScroll(scrollPos, introH);
+  });
+
+  function checkScroll(scrollPos, introH) {
     if (scrollPos > introH) {
       header.addClass("fixed");
     } else {
-      header.removeClass("fixed");
+      header.removeClas("fixed");
     }
+  }
+
+  /* Smooth scroll */
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offsent().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate(
+      {
+        scrillTop: elementOffset - 70,
+      },
+      700
+    );
+  });
+
+  /* Nov Toggle */
+
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+
+    nav.toggleClass("show");
   });
 });
